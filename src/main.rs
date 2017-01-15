02ead0f0097015a8af7673a8a260d49a43f9a202
Fernando Paredes <nano@fdp.io>
2017-01-14 22:29:49 -0800

    Read file into buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let file_name = env::args().nth(1).unwrap();
-    println!("{:?}", file_name);
+    let mut file = File::open(&file_name).unwrap();
+
+    let mut file_buffer = Vec::new();
+    file.read_to_end(&mut file_buffer).unwrap();
+
+    let file_buffer = file_buffer;
 }
