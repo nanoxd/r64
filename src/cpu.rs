8ad2508a5de40f2661cfa5832fa625f4b2b35e2f
Fernando Paredes <nano@fdp.io>
2017-01-15 14:08:52 -0800

    Move CPU code into own file

---
+++ src/cpu.rs
@@ -0,0 +1,70 @@
+const NUM_GPR: usize = 32;
+
+#[derive(Default, Debug)]
+pub struct CPU {
+    reg_grp: [u64; NUM_GPR],
+    reg_fpr: [u64; NUM_GPR],
+
+    reg_pc: u64,
+
+    reg_hi: u64,
+    reg_lo: u64,
+
+    reg_llbit: bool, // TODO: Enum type
+    reg_fcr0: u32,
+    reg_fcr31: u32,
+
+    cp0: CP0,
+}
+
+impl CPU {
+    pub fn power_on_reset(&mut self) {
+        self.cp0.power_on_reset();
+    }
+}
+
+// TODO: Better name
+#[derive(Debug)]
+enum RegConfigEp {
+    D,
+    DxxDxx,
+    RFU,
+}
+
+impl Default for RegConfigEp {
+    fn default() -> RegConfigEp { RegConfigEp::D }
+}
+
+#[derive(Debug)]
+enum RegConfigBe {
+    LittleEndian,
+    BigEndian,
+}
+
+impl Default for RegConfigBe {
+    fn default() -> RegConfigBe { RegConfigBe::BigEndian }
+}
+
+#[derive(Default, Debug)]
+struct RegConfig {
+    reg_config_ep: RegConfigEp,
+    reg_config_be: RegConfigBe,
+}
+
+impl RegConfig {
+    fn power_on_reset(&mut self) {
+        self.reg_config_ep = RegConfigEp::D;
+        self.reg_config_be = RegConfigBe::BigEndian;
+    }
+}
+
+#[derive(Default, Debug)]
+struct CP0 {
+    reg_config: RegConfig,
+}
+
+impl CP0 {
+    fn power_on_reset(&mut self) {
+        self.reg_config.power_on_reset();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,9 @@
+mod cpu;
+
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-
-const NUM_GPR: usize = 32;
-
-#[derive(Default, Debug)]
-struct CPU {
-    reg_grp: [u64; NUM_GPR],
-    reg_fpr: [u64; NUM_GPR],
-
-    reg_pc: u64,
-
-    reg_hi: u64,
-    reg_lo: u64,
-
-    reg_llbit: bool, // TODO: Enum type
-    reg_fcr0: u32,
-    reg_fcr31: u32,
-
-    cp0: CP0,
-}
-
-impl CPU {
-    fn power_on_reset(&mut self) {
-        self.cp0.power_on_reset();
-    }
-}
-
-// TODO: Better name
-#[derive(Debug)]
-enum RegConfigEp {
-    D,
-    DxxDxx,
-    RFU,
-}
-
-impl Default for RegConfigEp {
-    fn default() -> RegConfigEp { RegConfigEp::D }
-}
-
-#[derive(Debug)]
-enum RegConfigBe {
-    LittleEndian,
-    BigEndian,
-}
-
-impl Default for RegConfigBe {
-    fn default() -> RegConfigBe { RegConfigBe::BigEndian }
-}
-
-#[derive(Default, Debug)]
-struct RegConfig {
-    reg_config_ep: RegConfigEp,
-    reg_config_be: RegConfigBe,
-}
-
-impl RegConfig {
-    fn power_on_reset(&mut self) {
-        self.reg_config_ep = RegConfigEp::D;
-        self.reg_config_be = RegConfigBe::BigEndian;
-    }
-}
-
-#[derive(Default, Debug)]
-struct CP0 {
-    reg_config: RegConfig,
-}
-
-impl CP0 {
-    fn power_on_reset(&mut self) {
-        self.reg_config.power_on_reset();
-    }
-}
 
 fn main() {
     let pif_file_name = env::args().nth(1).unwrap();
@@ -81,7 +12,7 @@ fn main() {
     let pif = read_bin(pif_file_name);
     let rom = read_bin(rom_file_name);
 
-    let mut cpu = CPU::default();
+    let mut cpu = cpu::CPU::default();
     cpu.power_on_reset();
     print!("{:?}", &cpu);
 }
