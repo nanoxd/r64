25ac40b42b8a3a9a03523f722949fbc79d1e0f9a
Fernando Paredes <nano@fdp.io>
2017-01-15 09:16:49 -0800

    Create basic CPU data type

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,24 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+struct CPU {
+
+}
+
+impl CPU {
+    fn new() -> CPU {
+        CPU { }
+    }
+}
+
 fn main() {
     let pif_file_name = env::args().nth(1).unwrap();
     let rom_file_name = env::args().nth(2).unwrap();
 
     let pif = read_bin(pif_file_name);
     let rom = read_bin(rom_file_name);
+
+    let mut cpu = CPU::new();
 }
 
 fn read_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
