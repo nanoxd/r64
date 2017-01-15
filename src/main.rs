20e5ed44f3038cc7dcefb8e85a00c1f35ebb788c
Fernando Paredes <nano@fdp.io>
2017-01-15 09:06:49 -0800

    Extract opening rom to function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 
 fn main() {
     let file_name = env::args().nth(1).unwrap();
-    let mut file = File::open(&file_name).unwrap();
+}
 
+fn load_rom<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = File::open(&path).unwrap();
     let mut file_buffer = Vec::new();
     file.read_to_end(&mut file_buffer).unwrap();
 
-    let file_buffer = file_buffer;
+    file_buffer
 }
