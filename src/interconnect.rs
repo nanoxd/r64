4be0f6daccec86f7ab696605130304eaf9057134
Fernando Paredes <nano@fdp.io>
2017-01-15 17:53:17 -0800

    [Interconnect] Box up Ram, add Default

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -1,5 +1,13 @@
 const RAM_SIZE: usize = 4 * 1024 * 1024;
 
 pub struct Interconnect {
-    ram: [u16; RAM_SIZE],
+    ram: Box<[u16; RAM_SIZE]>,
+}
+
+impl Default for Interconnect {
+    fn default() -> Interconnect {
+        Interconnect {
+            ram: Box::new([0; RAM_SIZE])
+        }
+    }
 }
