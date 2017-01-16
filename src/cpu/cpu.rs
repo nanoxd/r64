7aae7ae88f81c4b44d2510305577d56b92f037c5
Fernando Paredes <nano@fdp.io>
2017-01-16 14:29:54 -0800

    Move cp0 into own module folder

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,5 +1,5 @@
 use ::interconnect;
-use super::cp0::CP0;
+use super::cp0::cp0::CP0;
 
 const NUM_GPR: usize = 32;
 
@@ -108,40 +108,5 @@ impl CPU {
         } else {
             panic!("Unrecognized virtual address: {:#x}", virt_addr);
         }
-    }
-}
-
-// TODO: Better name
-#[derive(Debug)]
-enum RegConfigEp {
-    D,
-    DxxDxx,
-    RFU,
-}
-
-impl Default for RegConfigEp {
-    fn default() -> RegConfigEp { RegConfigEp::D }
-}
-
-#[derive(Debug)]
-enum RegConfigBe {
-    LittleEndian,
-    BigEndian,
-}
-
-impl Default for RegConfigBe {
-    fn default() -> RegConfigBe { RegConfigBe::BigEndian }
-}
-
-#[derive(Debug, Default)]
-struct RegConfig {
-    reg_config_ep: RegConfigEp,
-    reg_config_be: RegConfigBe,
-}
-
-impl RegConfig {
-    fn power_on_reset(&mut self) {
-        self.reg_config_ep = RegConfigEp::D;
-        self.reg_config_be = RegConfigBe::BigEndian;
     }
 }
