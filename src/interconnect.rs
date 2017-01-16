c6764251132715850f4f7cf4a940828e8efad50a
Fernando Paredes <nano@fdp.io>
2017-01-15 21:19:33 -0800

    [Interconnect] Read word from pif_rom

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -13,4 +13,19 @@ impl Interconnect {
             ram: vec![0; RAM_SIZE]
         }
     }
+
+    pub fn read_word(&self, addr: u32) -> u32 {
+        // TODO: Replace constants
+        if addr >= 0x1fc0_0000 && addr < 0x1fc0_07c0 {
+            let rel_addr = addr - 0x1fc0_0000;
+
+            // TODO: Replace with byteorder crate
+            ((self.pif_rom[rel_addr as usize] as u32) << 24) |
+            ((self.pif_rom[(rel_addr + 1) as usize] as u32) << 16) |
+            ((self.pif_rom[(rel_addr + 2) as usize] as u32) << 8) |
+            (self.pif_rom[(rel_addr + 2) as usize] as u32)
+        } else {
+            panic!("Unrecognized virtual address {:#x}", addr)
+        }
+    }
 }
