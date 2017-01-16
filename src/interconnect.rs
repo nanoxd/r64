b352934a2384b1d0d90a7806fa1b182d2ddf0351
Fernando Paredes <nano@fdp.io>
2017-01-15 17:57:07 -0800

    [Interconnect] Use Vec to heap allocate

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -1,13 +1,13 @@
 const RAM_SIZE: usize = 4 * 1024 * 1024;
 
 pub struct Interconnect {
-    ram: Box<[u16; RAM_SIZE]>,
+    ram: Vec<u16>,
 }
 
 impl Default for Interconnect {
     fn default() -> Interconnect {
         Interconnect {
-            ram: Box::new([0; RAM_SIZE])
+            ram: vec![0; RAM_SIZE]
         }
     }
 }
