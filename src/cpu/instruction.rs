9a8194ea9bda8aaec22cf08393290363ab81a2ad
Fernando Paredes <nano@fdp.io>
2017-01-18 20:12:56 -0800

    [Instruction] Access the first element

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -1,16 +1,23 @@
+use super::opcode;
+
 pub struct Instruction(u32);
 
 impl Instruction {
+    pub fn opcode(&self) -> Opcode {
+        self.into()
+    }
+
     pub fn rs(&self) -> u32 {
-        (self >> 21) & 0b11111
+        (self.0 >> 21) & 0b11111
     }
 
     pub fn rt(&self) -> u32 {
-        (instruction >> 16) & 0b11111
+        (self.0 >> 16) & 0b11111
     }
 
+    #[inline(always)]
     pub fn imm(&self) -> u32 {
-        instruction & 0xffff
+        self.0 & 0xffff
     }
 
     pub fn offset(&self) -> u32 {
