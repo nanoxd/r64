94bf286f552db30edb23d89af2e4104d254d6179
Fernando Paredes <nano@fdp.io>
2017-01-18 20:14:11 -0800

    [Instruction] Inline
    not sure why yet

--- src/cpu/instruction.rs
+++ src/cpu/instruction.rs
@@ -3,14 +3,17 @@ use super::opcode;
 pub struct Instruction(u32);
 
 impl Instruction {
+    #[inline(always)]
     pub fn opcode(&self) -> Opcode {
         self.into()
     }
 
+    #[inline(always)]
     pub fn rs(&self) -> u32 {
         (self.0 >> 21) & 0b11111
     }
 
+    #[inline(always)]
     pub fn rt(&self) -> u32 {
         (self.0 >> 16) & 0b11111
     }
@@ -20,6 +23,7 @@ impl Instruction {
         self.0 & 0xffff
     }
 
+    #[inline(always)]
     pub fn offset(&self) -> u32 {
         self.imm()
     }
