572a5b1cf0a355484ecba0ae312135ea0e7e8c2e
Fernando Paredes <nano@fdp.io>
2017-01-21 19:18:58 -0800

    [RSP] read and write to status reg

--- src/rsp.rs
+++ src/rsp.rs
@@ -1,8 +1,42 @@
-#[derive(Default)]
-pub struct RSP;
+pub struct RSP {
+    halt: bool,
+    interrupt_enable: bool,
+    broke: bool,
+}
 
 impl RSP {
+    pub fn new() -> RSP {
+        RSP {
+            halt: true,
+            interrupt_enable: false,
+            broke: false
+        }
+    }
+
     pub fn read_status_reg(&self) -> u32 {
-        1
+        (if self.halt { 1 } else { 0 } << 0) |
+        (if self.interrupt_enable { 1 } else { 0 } << 1)
+    }
+
+    pub fn write_status_reg(&mut self, value: u32) {
+        // TODO: Add remaining bits
+        if (value & (1 << 0)) != 0 {
+            self.halt = false
+        }
+
+        if (value & (1 << 1)) != 0 {
+            self.halt = true
+        }
+
+        if (value & (1 << 2)) != 0 {
+            self.broke = false
+        }
+
+        if (value & (1 << 3)) != 0 {
+            self.interrupt_enable = false
+        }
+        if (value & 0xfffffff0) != 0 {
+            panic!("Writes to unsupported RSP status reg: {:#018X}", value);
+        }
     }
 }
