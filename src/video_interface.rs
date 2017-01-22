9f0be6a359ecddbce0374bc20042ac2c1cc1a1c8
Fernando Paredes <nano@fdp.io>
2017-01-22 11:16:29 -0800

    [VideoInterface] Model video start and end

--- src/video_interface.rs
+++ src/video_interface.rs
@@ -1,6 +1,9 @@
 #[derive(Default)]
 pub struct VideoInterface {
     interrupt_half_line: u32,
+
+    active_video_start: u32,
+    active_video_end: u32,
 }
 
 impl VideoInterface {
@@ -10,5 +13,14 @@ impl VideoInterface {
 
     pub fn write_intr_reg(&mut self, value: u32) {
         self.interrupt_half_line = value & 0x0000_03ff;
+    }
+
+    pub fn read_h_start_reg(&self) -> u32 {
+        (self.active_video_start << 16) | self.active_video_end
+    }
+
+    pub fn write_h_start_reg(&mut self, value: u32) {
+        self.active_video_start = (value >> 16) & 0x0000_03ff;
+        self.active_video_start = value & 0x0000_03ff;
     }
 }
