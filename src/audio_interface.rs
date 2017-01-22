c52916a4a625ba88a40e8379f3a4d891562b5d19
Fernando Paredes <nano@fdp.io>
2017-01-22 11:48:43 -0800

    [AudioInterface] Model length register

--- src/audio_interface.rs
+++ src/audio_interface.rs
@@ -1,6 +1,7 @@
 #[derive(Default)]
 pub struct AudioInterface {
     dram_addr_reg: u32,
+    length_reg: u32,
 }
 
 impl AudioInterface {
@@ -11,5 +12,12 @@ impl AudioInterface {
     pub fn write_dram_addr_reg(&mut self, value: u32) {
         self.dram_addr_reg = value & 0x00ff_ffff;
     }
+
+    pub fn read_len_reg(&self) -> u32 {
+        self.length_reg
+    }
+
+    pub fn write_len_reg(&mut self, value: u32) {
+        self.length_reg = value & 0x0003_fff8;
     }
 }
