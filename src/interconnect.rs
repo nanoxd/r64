85db02ae96fb4e56e75966f43f9ff08fa8e351e8
Fernando Paredes <nano@fdp.io>
2017-01-22 11:35:22 -0800

    [Interconnect] Handle beginnings of audio interface

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -4,11 +4,13 @@ use mem_map::*;
 use rsp::RSP;
 use peripheral_interface::PeripheralInterface;
 use video_interface::VideoInterface;
+use audio_interface::AudioInterface;
 
 const RAM_SIZE: usize = 4 * 1024 * 1024;
 
 pub struct Interconnect {
     pif_rom: Box<[u8]>,
+    ai: AudioInterface,
     pi: PeripheralInterface,
     vi: VideoInterface,
     ram: Box<[u16]>,
@@ -21,6 +23,7 @@ impl Interconnect {
             rsp: RSP::new(),
             pif_rom: pif_rom,
             ram: vec![0; RAM_SIZE].into_boxed_slice(),
+            ai: AudioInterface::default(),
             pi: PeripheralInterface::default(),
             vi: VideoInterface::default(),
         }
@@ -35,6 +38,7 @@ impl Interconnect {
             Addr::ViIntrReg => self.vi.read_intr_reg(),
             Addr::ViHStartReg => self.vi.read_h_start_reg(),
             Addr::ViCurrentReg => self.vi.read_current_reg(),
+            Addr::AiDramAddrReg => self.ai.read_dram_addr(),
         }
     }
 
@@ -47,6 +51,7 @@ impl Interconnect {
             Addr::ViIntrReg => self.vi.write_intr_reg(value),
             Addr::ViHStartReg => self.vi.write_h_start_reg(value),
             Addr::ViCurrentReg => self.vi.write_current_reg(value),
+            Addr::AiDramAddrReg => self.ai.write_dram_addr(value),
         }
     }
 }
--- src/mem_map.rs
+++ src/mem_map.rs
@@ -11,6 +11,9 @@ const VI_INTR_REG: u32 = 0x0440_000c;
 const VI_H_START_REG: u32 = 0x0440_0024;
 const VI_CURRENT_REG: u32 = 0x0440_0010;
 
+const AI_BASE_REG: u32 = 0x0450_0000;
+const AI_DRAM_ADDR_REG: u32 = AI_BASE_REG;
+
 const PI_BASE_REG: u32 = 0x0460_0000;
 const PI_STATUS_REG: u32 = 0x0460_0010;
 
@@ -24,6 +27,8 @@ pub enum Addr {
 
     SpStatusReg,
     SpDmaBusyReg,
+
+    AiDramAddrReg,
 }
 
 pub fn map_addr(addr: u32) -> Addr {
@@ -39,6 +44,8 @@ pub fn map_addr(addr: u32) -> Addr {
         VI_INTR_REG => Addr::ViIntrReg,
         VI_H_START_REG => Addr::ViHStartReg,
         VI_CURRENT_REG => Addr::ViCurrentReg,
+
+        AI_DRAM_ADDR_REG => Addr::AiDramAddrReg,
 
         _ => panic!("Unrecognized physical address {:#x}", addr)
     }
