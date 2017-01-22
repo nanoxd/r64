9d2d54467ba3c1fc205e5867036323ace050616a
Fernando Paredes <nano@fdp.io>
2017-01-21 21:08:49 -0800

    Handle pi status reg

--- src/interconnect.rs
+++ src/interconnect.rs
@@ -5,12 +5,6 @@ use rsp::RSP;
 
 const RAM_SIZE: usize = 4 * 1024 * 1024;
 
-enum Addr {
-    PifRom(u32),
-    SpStatusReg,
-    SpDmaBusyReg
-}
-
 pub struct Interconnect {
     pif_rom: Box<[u8]>,
     ram: Box<[u16]>,
@@ -27,30 +21,20 @@ impl Interconnect {
     }
 
     pub fn read_word(&self, addr: u32) -> u32 {
-        match self.mem_map(addr) {
+        match map_addr(addr) {
             Addr::PifRom(offset) => BigEndian::read_u32(&self.pif_rom[offset as usize..]),
             Addr::SpStatusReg => self.rsp.read_status_reg(),
             Addr::SpDmaBusyReg => self.rsp.read_dma_busy_reg(),
+            Addr::PiStatusReg => self.pi.read_status_reg(),
         }
     }
 
     pub fn write_word(&mut self, addr: u32, value: u32) {
-        match self.mem_map(addr) {
+        match map_addr(addr) {
             Addr::PifRom(_) => panic!("Cannot write to PIF ROM"),
             Addr::SpStatusReg => self.rsp.write_status_reg(value),
             Addr::SpDmaBusyReg => self.rsp.write_dma_busy_reg(value),
-        }
-    }
-
-    fn mem_map(&self, addr: u32) -> Addr {
-        match addr {
-            PIF_ROM_START ... PIF_ROM_END => Addr::PifRom(addr - PIF_ROM_START),
-
-            SP_STATUS_REG => Addr::SpStatusReg,
-
-            SP_DMA_BUSY_REG => Addr::SpDmaBusyReg,
-
-            _ => panic!("Unrecognized physical address {:#x}", addr)
+            Addr::PiStatusReg => self.pi.write_status_reg(value),
         }
     }
 }
--- src/mem_map.rs
+++ src/mem_map.rs
@@ -1,7 +1,32 @@
-pub const PIF_ROM_START: u32 = 0x1fc0_0000;
-pub const PIF_ROM_LENGTH: u32 = 2048;
-pub const PIF_ROM_END: u32 = PIF_ROM_START + PIF_ROM_LENGTH - 1;
+const PIF_ROM_START: u32 = 0x1fc0_0000;
+const PIF_ROM_LENGTH: u32 = 0x0000_07c0;
+const PIF_ROM_END: u32 = PIF_ROM_START + PIF_ROM_LENGTH - 1;
 
-pub const SP_BASE_REG: u32 = 0x0404_0000;
-pub const SP_STATUS_REG: u32 = 0x0404_0010;
-pub const SP_DMA_BUSY_REG: u32 = 0x0404_0018;
+const SP_BASE_REG: u32 = 0x0404_0000;
+const SP_STATUS_REG: u32 = 0x0404_0010;
+const SP_DMA_BUSY_REG: u32 = 0x0404_0018;
+
+const PI_BASE_REG: u32 = 0x0460_0000;
+const PI_STATUS_REG: u32 = 0x0460_0010;
+
+pub enum Addr {
+    PiStatusReg,
+    PifRom(u32),
+
+    SpStatusReg,
+    SpDmaBusyReg,
+}
+
+pub fn map_addr(addr: u32) -> Addr {
+    match addr {
+        PI_STATUS_REG => Addr::PiStatusReg,
+
+        PIF_ROM_START ... PIF_ROM_END => Addr::PifRom(addr - PIF_ROM_START),
+
+        SP_STATUS_REG => Addr::SpStatusReg,
+
+        SP_DMA_BUSY_REG => Addr::SpDmaBusyReg,
+
+        _ => panic!("Unrecognized physical address {:#x}", addr)
+    }
+}
