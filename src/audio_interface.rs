2f5856d021255db313d01f47d610a42e671141d0
Fernando Paredes <nano@fdp.io>
2017-01-22 11:31:17 -0800

    [AudioInterface] Model dram address

--- src/audio_interface.rs
+++ src/audio_interface.rs
@@ -1,1 +1,14 @@
-pub struct AudioInterface;
+#[derive(Default)]
+pub struct AudioInterface {
+    dram_addr: u32,
+}
+
+impl AudioInterface {
+    pub fn read_dram_addr(&self) -> u32 {
+        self.dram_addr
+    }
+
+    pub fn write_dram_addr(&mut self, value: u32) {
+        self.dram_addr = value & 0x00ff_ffff;
+    }
+}
