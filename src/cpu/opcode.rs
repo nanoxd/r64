7cdc0fe99e3b526d6ee1a56bf542d8311f45473f
Fernando Paredes <nano@fdp.io>
2017-02-02 17:31:32 -0800

    [Opcode] Add Special

--- src/cpu/opcode.rs
+++ src/cpu/opcode.rs
@@ -1,20 +1,21 @@
 enum_from_primitive! {
     #[derive(Debug)]
     pub enum Opcode {
-        Addi  = 0b001000,
-        Addiu = 0b001001,
+        Special = 0b000000,
+        Addi    = 0b001000,
+        Addiu   = 0b001001,
 
-        Andi  = 0b001100,
-        Ori   = 0b001101,
+        Andi    = 0b001100,
+        Ori     = 0b001101,
 
-        Lui   = 0b001111,
-        Mtc0  = 0b010000,
+        Lui     = 0b001111,
+        Mtc0    = 0b010000,
 
-        Beql  = 0b010100,
-        Bne   = 0b000101,
-        Bnel  = 0b010101,
+        Beql    = 0b010100,
+        Bne     = 0b000101,
+        Bnel    = 0b010101,
 
-        Lw    = 0b100011,
-        Sw    = 0b101011,
+        Lw      = 0b100011,
+        Sw      = 0b101011,
     }
 }
